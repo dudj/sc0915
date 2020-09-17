4a8dfe760c0df36716fac0eb487682911dce7e2e
dudj <13020078873@163.com>
2020-09-17 10:53:29 +0800

    init

---
+++ eureka/src/test/java/com/ld/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ld.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
