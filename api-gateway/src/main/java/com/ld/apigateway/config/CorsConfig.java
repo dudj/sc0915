b7e3efe969b37c965d5d6161e7471e3457553ab8
dudj <13020078873@163.com>
2020-10-30 17:12:21 +0800

    zuul跨域 几种方式 1.配置所有(zuul) 2.单独在某方法上

---
+++ api-gateway/src/main/java/com/ld/apigateway/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.ld.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+/**
+ * 跨域配置
+ * cors Cross Origin Resource Sharing
+ * 跨域资源共享
+ */
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setMaxAge(300L);
+        source.registerCorsConfiguration("/**",config);
+        return new CorsFilter();
+    }
+}
