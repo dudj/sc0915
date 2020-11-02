6cab119444fab211c084e5d9d60e401ad249c3f7
dudj <13020078873@163.com>
2020-11-02 15:42:21 +0800

    hystrix熔断，界面管理使用(dashboard)

--- order/server/src/main/java/com/ld/order/OrderApplication.java
+++ order/server/src/main/java/com/ld/order/OrderApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.ComponentScan;
 
 //@SpringBootApplication
 //@EnableDiscoveryClient
@@ -19,6 +21,8 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
  * @EnableCircuitBreaker
  * 这三个
  */
+@ComponentScan(basePackages = "com.ld")
+@EnableHystrixDashboard
 public class OrderApplication {
 
     public static void main(String[] args) {
--- product/client/src/main/java/com/ld/product/client/ProductClient.java
+++ product/client/src/main/java/com/ld/product/client/ProductClient.java
@@ -3,15 +3,31 @@ package com.ld.product.client;
 import com.ld.product.common.DeductStockInPut;
 import com.ld.product.common.ProductInfoOutPut;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "product")
+@FeignClient(name = "product",fallback = ProductClient.ProductClientFallback.class)
 public interface ProductClient {
     @PostMapping("/product/listForOrder")
     List<ProductInfoOutPut> listForOrder(@RequestBody List<String> productIdList);
     @PostMapping("/product/deductStock")
     void deductStock(@RequestBody List<DeductStockInPut> deductStockInPutList);
+
+    //外部调用 如果出错 ，会降级
+    @Component
+    static class ProductClientFallback implements ProductClient{
+
+        @Override
+        public List<ProductInfoOutPut> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void deductStock(List<DeductStockInPut> deductStockInPutList) {
+
+        }
+    }
 }
